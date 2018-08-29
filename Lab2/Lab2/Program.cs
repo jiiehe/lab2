613ae3555f4d26b21776e11b1e1c061f30c1258f
Jin He <1173311048@qq.com>
2018-08-29 02:51:26 -0600

    get try

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -11,6 +11,7 @@ namespace Lab2
         static void Main(string[] args)
         {
             Console.WriteLine("hahaha");
+            Console.Write("ddd");
             Console.Read();
         }
     }
