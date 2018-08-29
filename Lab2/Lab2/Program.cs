1c0c6bcf394f280d2b1fe76f33cd844dfb2c27dd
Jin He <1173311048@qq.com>
2018-08-29 02:33:42 -0600

    Revert "Revert "Creat Lab2 Solution""
    This reverts commit 43c19d82ea185db36f4a25176c814b119283b062.

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        /// <summary>
+        /// this is test
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.Read();
+        }
+    }
+}
